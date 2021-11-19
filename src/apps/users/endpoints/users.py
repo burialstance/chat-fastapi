f44f023f925788b8f9fbb9fcd5048b0a22fa85b3
burialstance <burialstance@gmail.com>
2021-11-19 17:06:52 +0500

    modify users router

--- src/apps/users/endpoints/users.py
+++ src/apps/users/endpoints/users.py
@@ -7,14 +7,9 @@ from src.apps.users.models.user import User
 router = APIRouter()
 
 
-@router.get('/', response_model=schemas.UserPublic)
+@router.get('/{id}', response_model=schemas.UserPublic)
 async def get_user(id: int):
     return await User.get(id=id)
-
-
-@router.post('/', response_model=schemas.UserPublic)
-async def create_user(form: schemas.UserCreate):
-    return await User.create(**form.dict(exclude_unset=True))
 
 
 @router.post('/signup', response_model=schemas.UserPublic)
