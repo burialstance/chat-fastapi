0cc27f61e0cf4578d88bfbc42077a0f57911b464
burialstance <burialstance@gmail.com>
2021-11-19 17:04:20 +0500

    schemas fix

--- src/apps/profiles/schemas.py
+++ src/apps/profiles/schemas.py
@@ -5,15 +5,18 @@ from src.conf.enums import GendersEnum
 
 
 class ProfileBase(BaseModel):
-    user_id: int
     age: Optional[int]
     gender: Optional[GendersEnum]
     country_id: Optional[int]
 
 
 class ProfileCreate(ProfileBase):
-    ...
+    user_id: int
 
 
 class ProfilePublic(ProfileBase):
+    user_id: int
+
+
+class ProfileUpdate(ProfileBase):
     ...
--- src/apps/search_options/schemas.py
+++ src/apps/search_options/schemas.py
@@ -5,18 +5,20 @@ from pydantic import BaseModel
 from src.conf.enums import GendersEnum
 
 
-
 class SearchOptionsBase(BaseModel):
-    user_id: int
     from_age: Optional[int]
     to_age: Optional[int]
     gender: Optional[GendersEnum]
     country_id: Optional[int]
 
 
-class SearchOptionsPublic(SearchOptionsBase):
-    ...
-
-
 class SearchOptionsCreate(SearchOptionsBase):
+    user_id: int
+
+
+class SearchOptionsPublic(SearchOptionsBase):
+    user_id: int
+
+
+class SearchOptionsUpdate(SearchOptionsBase):
     ...
