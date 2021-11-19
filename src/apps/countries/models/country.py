f4ccf68fbecfee6bc71298793c57553445594722
burialstance <burialstance@gmail.com>
2021-11-19 19:40:49 +0500

    add pydantic meta to country model

--- src/apps/countries/models/country.py
+++ src/apps/countries/models/country.py
@@ -6,3 +6,8 @@ from tortoise import fields, models
 class Country(models.Model):
     name: str = fields.CharField(max_length=64, unique=True)
     icon: Optional[str] = fields.CharField(max_length=8, null=True)
+
+    class PydanticMeta:
+        exclude = (
+            'profiles', 'search_options'
+        )
