cf8ba464c4e386fb07f0a9a5805243a6c1e2d140
burialstance <burialstance@gmail.com>
2021-11-22 14:11:38 +0500

    include messenger routers to main app router

--- src/apps/router.py
+++ src/apps/router.py
@@ -4,6 +4,7 @@ from src.apps.users.endpoints import users
 from src.apps.countries.endpoints import countries
 from src.apps.profiles.endpoints import profiles
 from src.apps.search_options.endpoints import search_options
+from src.apps.messengers.endpoints import messengers
 
 router = APIRouter(prefix='/api')
 
@@ -11,3 +12,5 @@ router.include_router(users.router, prefix='/users', tags=['users'])
 router.include_router(profiles.router, prefix='/profiles', tags=['profiles'])
 router.include_router(countries.router, prefix='/countries', tags=['countries'])
 router.include_router(search_options.router, prefix='/search_options', tags=['search_options'])
+router.include_router(messengers.router, prefix='/messengers', tags=['messengers'])
+
